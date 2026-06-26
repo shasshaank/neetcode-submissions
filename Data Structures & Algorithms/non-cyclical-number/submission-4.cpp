b026cf7559550d7fe1ad32df84d1d984aaee957d
Shashaank <111143497+shasshaank@users.noreply.github.com>
2026-06-26 22:07:32 +0530

    Add: non-cyclical-number - submission-4

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int sum = 0;
+        while(n>0){
+            sum+= pow(n%10,2);
+            n = n/10;
+        }
+
+        if(sum==1 || sum==7){
+            return true;
+        }else if(sum<10){
+            return false;
+        }else{
+            return isHappy(sum);
+        }
+
+        return false;
+    }
+};
